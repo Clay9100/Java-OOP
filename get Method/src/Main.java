8aab7ba9a757a9e69300b4f6a15fb632541c2a59
Muhammadsobir <muhammadsobirturdaliyev9199@gmail.com>
2025-07-26 09:59:45 +0500

    added new lesson create name get Method

---
+++ get Method/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // this is lesson getMethod()
+
+        int age = getAge();
+        System.out.println("Your age is: " + age);
+
+    }
+    public static int getAge(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int age = input.nextInt();
+        return age;
+    }
+
+
+
+}
