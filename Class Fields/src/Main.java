3c83b70008cf5648325515231c0f729e9df7e0af
Muhammadsobir <muhammadsobirturdaliyev9199@gmail.com>
2025-07-27 11:06:15 +0500

    added create User Objects

--- Class Fields/src/Main.java
+++ Class Fields/src/Main.java
@@ -5,10 +5,19 @@ public class Main {
 
         // this is lesson class fields
 
-        int n = 89;
+       /* int n = 89;
 
         String str = "Hello";
-        String strObj = new String("Hello");
+        String strObj = new String("Hello");*/
+
+        User user1 = new User();
+        user1.name = "Ali";
+        user1.surname = "Aliyev";
+        user1.age = 23;
+
+        User user2 = new User();
+        User user3 = new User();
+
 
 
 
