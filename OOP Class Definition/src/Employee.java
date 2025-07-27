51b9db040738391dc4ad6a014f6203f59a73b731
Muhammadsobir <muhammadsobirturdaliyev9199@gmail.com>
2025-07-27 14:05:36 +0500

    added create new lesson class Definition

---
+++ OOP Class Definition/src/Employee.java
@@ -0,0 +1,13 @@
+public class Employee {
+
+    String name;
+
+
+    double salary;
+
+
+    @Override
+    public String toString() {
+        return name + " " +  salary;
+    }
+}
---
+++ OOP Class Definition/src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // this is lesson OOP Class definition
+
+        Employee employee = new Employee();
+        employee.name = "Ali";
+        employee.salary = 4500;
+
+        System.out.println("employee = " + employee);
+
+
+
+    }
+}
+
