34d43fbc33ee5849d301acc8b4f249e8e78889c2
Muhammadsobir <muhammadsobirturdaliyev9199@gmail.com>
2025-07-27 12:45:07 +0500

    added create new user1 object

--- Several Object/src/Main.java
+++ Several Object/src/Main.java
@@ -5,6 +5,15 @@ public class Main {
 
         // this is lesson several objects
 
+        // create objects more
+
+        User user1 = new User();
+        user1.name = "Ali";
+        user1.surname = "Aliyev";
+        user1.age = 23;
+
+        System.out.println(user1.name + " " +  user1.surname + " " + user1.age);
+
 
     }
 }
